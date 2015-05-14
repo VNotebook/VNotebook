0f8d5ac07b3cfe6ed68fdca937bee5413c8456fa
Daniel Castro Alvarado <dancas2@gmail.com>
2015-05-14 02:37:13 -0500

    Updating Travis CI configuration to run Angular unit tests

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -1,3 +1,9 @@
 ./grailsw refresh-dependencies
 ./grailsw compile
 ./grailsw test-app
+
+cd angular-app
+npm install bower karma-cli
+npm install
+bower install
+karma start --single-run --browsers PhantomJS test/karma/karma.conf.js
