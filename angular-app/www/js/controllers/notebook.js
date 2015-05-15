7eb5c0b8798d615e299aa9c2361c26b355be9203
Daniel Castro Alvarado <dancas2@gmail.com>
2015-05-15 11:38:49 -0500

    The notebook controller and template are now using the UserIsLeftHanded configuration

--- angular-app/www/js/controllers/notebook.js
+++ angular-app/www/js/controllers/notebook.js
@@ -1,52 +1,45 @@
 'use strict';
 
-application.controller('NotebookController', function($scope, $location, $routeParams, Elements) {
+application.controller('NotebookController', function($scope, $location,
+  $routeParams, Elements, appConfig) {
+  var initialLeftPanel = [
+    {
+      title: "Widget 1",
+      buttonClass: "glyphicon glyphicon-camera",
+      action: $scope.doNothing
+    },
+    {
+      title: "Widget 2",
+      buttonClass: "glyphicon glyphicon-edit",
+      action: $scope.doNothing
+    }
+  ];
 
-    $scope.changePosition = function () {
-        var temp = $scope.leftPanel;
-        $scope.leftPanel = $scope.rightPanel;
-        $scope.rightPanel = temp;
-    };
+  var initialRightPanel = [
+    {
+      title: "Editar",
+      buttonClass: "glyphicon glyphicon-pencil",
+      action: $scope.doNothing
+    },
+    {
+      title: "Borrar",
+      buttonClass: "glyphicon glyphicon-erase",
+      action: $scope.doNothing
+    }
+  ];
 
-    $scope.doNothing = function () {
-        // do something here
-    };
+  $scope.leftPanel = initialLeftPanel;
+  $scope.rightPanel = initialRightPanel;
 
-    $scope.swap = {
-        title: "Cambiar",
-        buttonClass: "glyphicon glyphicon-transfer",
-        action: $scope.changePosition
-    };
-
-    $scope.leftPanel = [
-        {
-            title: "Widget 1",
-            buttonClass: "glyphicon glyphicon-camera",
-            action: $scope.doNothing
-        },
-        {
-            title: "Widget 2",
-            buttonClass: "glyphicon glyphicon-edit",
-            action: $scope.doNothing
-        }
-    ];
-    $scope.rightPanel = [
-        {
-            title: "Editar",
-            buttonClass: "glyphicon glyphicon-pencil",
-            action: $scope.doNothing
-        },
-        {
-            title: "Borrar",
-            buttonClass: "glyphicon glyphicon-erase",
-            action: $scope.doNothing
-        }
-    ];
-
-    var load = function () {
-
-        // Load something
-    };
-
-    load();
+  $scope.$watch(function() {
+    return appConfig.getUserIsLeftHanded();
+  }, function(newValue) {
+    if (newValue) {
+      $scope.leftPanel = initialRightPanel;
+      $scope.rightPanel = initialLeftPanel;
+    } else {
+      $scope.leftPanel = initialLeftPanel;
+      $scope.rightPanel = initialRightPanel;
+    }
+  });
 });
