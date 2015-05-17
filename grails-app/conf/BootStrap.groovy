c4a53a7659b2fbf20d713335285d32c7a3946858
Daniel Castro Alvarado <dancas2@gmail.com>
2015-05-17 01:10:31 -0500

    Adding sample administrator on bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,14 +5,21 @@ import vnotebook.UserRole
 class BootStrap {
 
     def init = { servletContext ->
+        // Roles setup
+        Role roleUser = new Role(authority: "ROLE_USER")
+        roleUser.save()
+        Role roleAdmin = new Role(authority: "ROLE_ADMIN")
+        roleAdmin.save()
+
         User user = new User(username: "test", password: "test123", accountExpired: false, email: "test@test.com",
                 firstName: "Test", lastName: "VNoteBook")
         user.save()
-
-        Role roleUser = new Role(authority: "ROLE_USER")
-        roleUser.save()
+        User admin = new User(username: "admin", password: "admin123", accountExpired: false, email: "admin@test.com",
+                firstName: "Admin", lastName: "VNoteBook")
+        admin.save()
 
         new UserRole(user: user, role: roleUser).save()
+        new UserRole(user: admin, role: roleAdmin).save()
     }
     def destroy = {
     }
