08665dbc802aaebbe50b1804243673fd08b01018
Daniel Castro Alvarado <dancas2@gmail.com>
2015-05-17 14:06:57 -0500

    Fixing login e2e test

--- angular-app/test/e2e/core.js
+++ angular-app/test/e2e/core.js
@@ -2,6 +2,12 @@ describe("Core functionality", function() {
   it("should allow users to login", function() {
     browser.get("");
 
+    // Cancel initial dialog, dismiss alert and trigger login manually
+    element(by.buttonText("Cerrar")).click();
+    browser.switchTo().alert().then(function(alert) {
+      return alert.dismiss();
+    });
+
     var loginButton = element(by.buttonText('Ingresar'));
     loginButton.click();
 
