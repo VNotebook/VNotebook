1ab8b13f8d74518293093691904a6092bcd78273
Milder Hernandez <milderhc@gmail.com>
2015-06-05 00:16:22 -0500

    Add and update managed by the same dialog and controller

--- web-app/js/controllers/eventEditorDialog.js
+++ web-app/js/controllers/eventEditorDialog.js
@@ -1,10 +1,34 @@
 application.controller('EventEditorDialogController', function($scope, $http, apiUrl) {
-	$scope.newEvent = {
-		title: 'Nuevo Evento',
-		type: 'Información',
+	$scope.event = {
+		title: "Nuevo evento",
+		type: "Información",
 		startsAt: new Date(),
 		endsAt: new Date()
 	};
+	$scope.newEvent = true;
+
+	var update = function() {
+		$scope.errors = null;
+	    $http.put(apiUrl + "/events/" + $scope.event.id, $scope.event).then(function() {
+	      $scope.$close();
+	    }, function(response) {
+	      $scope.errors = response.data && response.data['errors'];
+	    });
+	}
+
+	var add = function() {
+		$scope.errors = null;
+	    $http.post(apiUrl + "/events", $scope.event).then(function() {
+	      $scope.$close();
+	    }, function(response) {
+	      $scope.errors = response.data && response.data['errors'];
+	    });
+	}
+
+	$scope.init = function(event) {
+		$scope.event = event;
+		$scope.newEvent = false;
+	}
 
 	$scope.toggle = function($event, field, event) {
 		$event.preventDefault();
@@ -13,13 +37,12 @@ application.controller('EventEditorDialogController', function($scope, $http, ap
 	};
 
 	$scope.save = function() {
-		$scope.errors = null;
-		$scope.newEvent.startsAt = $scope.newEvent.startsAt.getTime();
-		$scope.newEvent.endsAt = $scope.newEvent.endsAt.getTime();
-	    $http.post(apiUrl + "/events", $scope.newEvent).then(function() {
-	      $scope.$close();
-	    }, function(response) {
-	      $scope.errors = response.data && response.data['errors'];
-	    });
+		$scope.event.startsAt = $scope.event.startsAt.getTime();
+		$scope.event.endsAt = $scope.event.endsAt.getTime();
+		if ( $scope.newEvent == true ) {
+			add();
+		} else {
+			update();
+		}
 	};
 });
