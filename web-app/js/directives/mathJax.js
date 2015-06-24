abe969e4db07193d4deb3daf1f116b4e69629331
JeffersonH44 <swrt_9950@hotmail.com>
2015-06-24 14:22:14 -0500

    Mathjax configuration inside of the mathjaxBind directive.

--- web-app/js/directives/mathJax.js
+++ web-app/js/directives/mathJax.js
@@ -1,16 +1,16 @@
-MathJax.Hub.Config({
-    extensions: ["tex2jax.js"],
-    jax: ["input/TeX", "output/HTML-CSS"],
-    tex2jax: {
-        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-        displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
-        processEscapes: true
-    },
-    "HTML-CSS": { availableFonts: ["TeX"] }
-});
-MathJax.Hub.Configured();
-
 application.directive("mathjaxBind", function() {
+    MathJax.Hub.Config({
+        extensions: ["tex2jax.js"],
+        jax: ["input/TeX", "output/HTML-CSS"],
+        tex2jax: {
+            inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+            displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+            processEscapes: true
+        },
+        "HTML-CSS": { availableFonts: ["TeX"] }
+    });
+    MathJax.Hub.Configured();
+
     return {
         restrict: "A",
         controller: ["$scope", "$element", "$attrs", function($scope, $element, $attrs) {
